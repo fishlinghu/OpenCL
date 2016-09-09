d30a2427395d65fd2f25563681a287b5ef226ed6
fishlinghu <fishlinghu@gmail.com>
2016-09-09 01:46:52 -0400

    Get Platform and Device Info

--- get_device_info.cpp
+++ get_device_info.cpp
@@ -5,12 +5,43 @@
 #endif
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 
 #define DATA_SIZE 1000000
 
 using namespace std;
+
+cl_program load_program(cl_context context, const char* filename)
+    {
+    ifstream in(filename, ios_base::binary);
+    if(!in.good()) 
+        {
+        return 0;
+        }
+
+    // get file length
+    in.seekg(0, ios_base::end);
+    size_t length = in.tellg();
+    in.seekg(0, ios_base::beg);
+
+    // read program source
+    vector<char> data(length + 1);
+    in.read(&data[0], length);
+    data[length] = 0;
+
+    // create and build program 
+    const char* source = &data[0];
+    cl_program program = clCreateProgramWithSource(context, 1, &source, 0, 0);
+    if(program == 0) 
+        return 0;
+
+    if(clBuildProgram(program, 0, 0, 0, 0, 0) != CL_SUCCESS) 
+        return 0;
+
+    return program;
+    }
 
 int main()
     {
@@ -30,7 +61,7 @@ int main()
         std::cerr << "Unable to get platform ID\n";
         return 0;
         }
-
+    // Create context
     cl_context_properties prop[] = { CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties>(platforms[0]), 0 };
     cl_context context = clCreateContextFromType(prop, CL_DEVICE_TYPE_DEFAULT, NULL, NULL, NULL);
     if(context == 0) 
@@ -82,6 +113,64 @@ int main()
         return 0;
        }
 
+    cl_program program = load_program(context, "shader.cl");
+    if(program == 0) 
+        {
+        cerr << "Can't load or build program\n";
+        clReleaseMemObject(cl_a);
+        clReleaseMemObject(cl_b);
+        clReleaseMemObject(cl_res);
+        clReleaseCommandQueue(queue);
+        clReleaseContext(context);
+        return 0;
+        }
+    // Access the adder function
+    cl_kernel adder = clCreateKernel(program, "adder", 0);
+    if(adder == 0) 
+        {
+        cerr << "Can't load kernel\n";
+        clReleaseProgram(program);
+        clReleaseMemObject(cl_a);
+        clReleaseMemObject(cl_b);
+        clReleaseMemObject(cl_res);
+        clReleaseCommandQueue(queue);
+        clReleaseContext(context);
+        return 0;
+        }
+
+    clSetKernelArg(adder, 0, sizeof(cl_mem), &cl_a);
+    clSetKernelArg(adder, 1, sizeof(cl_mem), &cl_b);
+    clSetKernelArg(adder, 2, sizeof(cl_mem), &cl_res);
+
+    size_t work_size = DATA_SIZE;
+    err = clEnqueueNDRangeKernel(queue, adder, 1, 0, &work_size, 0, 0, 0, 0);
+    if(err==CL_SUCCESS)
+        clEnqueueReadBuffer(queue, cl_res, CL_TRUE, 0, sizeof(float) * DATA_SIZE, &res[0], 0, 0, 0);    
+
+    if(err == CL_SUCCESS) 
+        {
+        bool correct = true;
+        for(int i = 0; i < DATA_SIZE; i++) 
+            {
+            if(a[i] + b[i] != res[i]) 
+                {
+                correct = false;
+                break;
+                }
+            }
+
+    if(correct) 
+        cout << "Data is correct\n";
+    else 
+        cout << "Data is incorrect\n";
+
+        }
+    else 
+        cerr << "Can't run kernel or read back data\n";
+    
+
+    clReleaseKernel(adder);
+    clReleaseProgram(program);
     clReleaseCommandQueue(queue);
     clReleaseContext(context);
 
---
+++ list_platform.cpp
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+ 
+int main() 
+    {
+    int i, j, k;
+    char* info;
+    size_t infoSize;
+    cl_uint platformCount; // get the # of platforms available
+    cl_platform_id *platforms;
+    const char* attributeNames[5] = { "Name", "Vendor", "Version", "Profile", "Extensions" };
+    // A platform only gets these 5 types of attributes, and we have to follow the naming rule
+    const cl_platform_info attributeTypes[5] = { CL_PLATFORM_NAME, CL_PLATFORM_VENDOR, CL_PLATFORM_VERSION, CL_PLATFORM_PROFILE, CL_PLATFORM_EXTENSIONS };
+    const int attributeCount = sizeof(attributeNames) / sizeof(char*);
+ 
+    // get platform count
+    clGetPlatformIDs(5, NULL, &platformCount);
+    // get all platforms
+    platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * platformCount);
+    clGetPlatformIDs(platformCount, platforms, NULL);
+ 
+    // for each platform print all attributes
+    for (i = 0; i < platformCount; i++) 
+        {
+        printf("\n %d. Platform \n", i+1);
+ 
+        for (j = 0; j < attributeCount; j++) 
+            {
+            // attributeCount == 5 here
+            // get platform attribute value size
+            clGetPlatformInfo(platforms[i], attributeTypes[j], 0, NULL, &infoSize);
+            info = (char*) malloc(infoSize);
+ 
+            // get platform attribute value
+            clGetPlatformInfo(platforms[i], attributeTypes[j], infoSize, info, NULL);
+ 
+            printf("  %d.%d %-11s: %s\n", i+1, j+1, attributeNames[j], info);
+            free(info);
+            }
+        printf("\n");
+        }
+    ////
+    char* value;
+    size_t valueSize;
+    cl_uint deviceCount;
+    cl_device_id* devices;
+    
+    const char* device_attributeNames[5] = { "Device", "Vendor", "Hardware version", "Software version", "OpenCL C version"};
+    const cl_device_info device_attributeTypes[5] = { CL_DEVICE_NAME, CL_DEVICE_VENDOR, CL_DEVICE_VERSION, CL_DRIVER_VERSION, CL_DEVICE_OPENCL_C_VERSION};
+    const int device_attributeCount = sizeof(device_attributeNames) / sizeof(char*);
+
+    cl_uint clAttrUnits;
+    const char* device_attributeNames_2[3] = { "Global Memory Cacheline Size", "Max Clock Freq", "Max Compute Units"};
+    const cl_device_info device_attributeTypes_2[3] = { 
+        CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE, 
+        CL_DEVICE_MAX_CLOCK_FREQUENCY, 
+        CL_DEVICE_MAX_COMPUTE_UNITS};
+    const int device_attributeCount_2 = sizeof(device_attributeNames_2) / sizeof(char*);
+ 
+    for (i = 0; i < platformCount; i++) 
+        {
+        // for each platform, get all devices on the platform
+        // last 3 parameters are like the parameters in the clGetPlatformIDs
+        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
+        devices = (cl_device_id*) malloc(sizeof(cl_device_id) * deviceCount);
+        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+ 
+        // for each device print critical attributes
+        for (j = 0; j < deviceCount; j++) 
+            {
+            /*
+            // print device name
+            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+            printf("%d. Device: %sn", j+1, value);
+            free(value);
+ 
+            // print hardware device version
+            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
+            printf(" %d.%d Hardware version: %sn", j+1, 1, value);
+            free(value);
+ 
+            // print software driver version
+            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
+            printf(" %d.%d Software version: %sn", j+1, 2, value);
+            free(value);
+ 
+            // print c version supported by compiler for device
+            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+            printf(" %d.%d OpenCL C version: %sn", j+1, 3, value);
+            free(value);
+ 
+            // print parallel compute units
+            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+            printf(" %d.%d Parallel compute units: %dn", j+1, 4, maxComputeUnits);
+            */
+            for (k = 0; k < device_attributeCount; k++)
+                {
+                clGetDeviceInfo(devices[j], device_attributeTypes[k], 0, NULL, &valueSize);
+                value = (char*) malloc(valueSize);
+                clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+                printf("%d.%d %s: %s\n", j+1, k, device_attributeNames[k], value);
+                free(value);
+                }
+            for (k = 0; k < device_attributeCount_2; k++)
+                {
+                clGetDeviceInfo(devices[j], device_attributeTypes_2[k], sizeof(clAttrUnits), &clAttrUnits, NULL);
+                printf("%d.%d %s: %d\n", j+1, k+device_attributeCount, device_attributeNames_2[k], clAttrUnits);
+                }
+            }
+        free(devices);
+        }
+
+    ////
+    free(platforms);
+    return 0;
+    }
